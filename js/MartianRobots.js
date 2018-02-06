04cd9db7e216ce5a4746e710ff3428214eb4d7b2
ashil88 <ashil88@hotmail.com>
2018-02-06 17:32:30 +0000

    Setting mars bounds coordinates

--- js/MartianRobots.js
+++ js/MartianRobots.js
@@ -1,7 +1,17 @@
 /// <reference path="jquery.d.ts" />
 class MartianRobotsManager {
     constructor(martiansWrapper) {
-        console.log(martiansWrapper);
+        this.coordinatesForm = martiansWrapper.find('#form');
+        this.marsBounds = [];
+        this.coordinatesForm.on('submit', (e) => {
+            e.preventDefault();
+            this.submitCoordinatesForm();
+        });
+    }
+    submitCoordinatesForm() {
+        let coordinate_x = parseFloat($('#coord_x').val().toString()), coordinate_y = parseFloat($('#coord_y').val().toString());
+        this.marsBounds = [coordinate_x, coordinate_y];
+        console.log(this.marsBounds);
     }
 }
 const martiansWrapper = $('.martians-wrapper');
--- js/MartianRobots.ts
+++ js/MartianRobots.ts
@@ -2,9 +2,27 @@
 
 class MartianRobotsManager {
 
+    private coordinatesForm: JQuery;
+    private marsBounds: Array<number>;
+
     constructor(martiansWrapper: JQuery) {
 
-        console.log(martiansWrapper);
+        this.coordinatesForm = martiansWrapper.find('#form');
+        this.marsBounds = [];
+
+        this.coordinatesForm.on('submit', (e) => {
+            e.preventDefault();
+            this.submitCoordinatesForm();
+        });
+    }
+
+    private submitCoordinatesForm() {
+        let coordinate_x = parseFloat($('#coord_x').val().toString()),
+            coordinate_y = parseFloat($('#coord_y').val().toString());
+
+        this.marsBounds = [coordinate_x, coordinate_y];
+
+        console.log(this.marsBounds);
     }
 
 }
